0f3e1531d9460778c2712215166b45df05f50b03
tomasmazzocchi <36847481+tomasmazzocchi@users.noreply.github.com>
2020-03-14 18:33:15 -0300

    Cambio a component ya que no es interfaz

--- src/main/java/com/mycompany/wsschallengesantander/client/MetereologiaClient.java
+++ src/main/java/com/mycompany/wsschallengesantander/client/MetereologiaClient.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
+@Component
 public class MetereologiaClient  {
     
     @Autowired
